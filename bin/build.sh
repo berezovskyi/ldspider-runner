30e9fe97d67bfc1b55e226b19b392ecfb0ecebc4
Andrew Berezovskyi <andriib@kth.se>
2017-04-14 22:08:01 +0200

    add build comments

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,7 @@
 set -eu
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 mvn -version
+# just in case you didn't bother to read the README
 (cd "$SCRIPT_DIR/../" && git submodule update --init --recursive)
+# see https://github.com/ldspider/ldspider/issues/10
 (cd "$SCRIPT_DIR/../ldspider" && mvn clean package -Dmaven.test.skip=true)

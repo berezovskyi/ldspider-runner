4392603f87b5b69bd39562b9576d6369382d1351
Andrew Berezovskyi <andriib@kth.se>
2017-04-14 22:00:27 +0200

    Update submodules automatically

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,5 @@
 set -eu
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 mvn -version
+(cd "$SCRIPT_DIR/../" && git submodule update --init --recursive)
 (cd "$SCRIPT_DIR/../ldspider" && mvn clean package -Dmaven.test.skip=true)
